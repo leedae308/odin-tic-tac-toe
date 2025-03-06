4a7060f9e4555968ee81beccd4c434c8d5f0f428
Daehee Lee <daehee.lee308@gmail.com>
2025-03-06 21:19:01 +0900

    finish HTML main page + css design
    need work: displayModule, connecting game js with
    display.

---
+++ js/displayModule.js
@@ -0,0 +1,36 @@
+(function () {
+    var displayModule = {
+        init: function(){
+            this.cacheDOM();
+            this.attachEvents();
+            this.game=PlayGame("player1", "player2");
+        },
+
+        cacheDOM: function(){
+            this.redoBtn = document.querySelector(".redo");
+            this.gameBoxes = document.querySelectorAll(".gameBox");
+
+
+        },
+        attachEvents: function(){
+            this.redoBtn.addEventListener("click", ()=>{
+                this.gameBoxes.forEach((element)=>{
+                    element.innerHTML = "";
+                    this.game.playRound();
+                });
+            });
+
+            this.gameBoxes.forEach((element)=>{
+                element.addEventListener("click", (event)=>{
+                    alert([event.target.getAttribute("row"), event.target.getAttribute("col")]);
+                })
+            })
+
+        },
+
+
+
+    };
+
+    displayModule.init();
+})();
