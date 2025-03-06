0f164d513c2669f7525c41325416499360f14653
Daehee Lee <daehee.lee308@gmail.com>
2025-03-06 18:50:42 +0900

    fix again problem
    the gameBoard.js reference was flipped :(

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -2,7 +2,7 @@ var GameBoard = (function () {
     return function createGameBoard() {
         var gameBoard = Array.from({ length: 3 }, () => new Array(3).fill(null));
 
-        const setMarker = function (marker, [col, row]) {
+        const setMarker = function (marker, [row, col]) {
             gameBoard[row][col] = marker;
         };
         const resetGameBoard = function () {
